c8e4c4c14743090d0da6e9148121f6d5c359d769
EnriqueIrazoqui <za19012238@zapopan.tecmm.edu.mx>
2026-03-14 03:23:38 -0600

    docs: add swagger documentation for health and auth

--- src/app/app.js
+++ src/app/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
 
 const routes = require("../routes");
+const swaggerSpec = require("../docs/swagger");
 const notFoundMiddleware = require("../middlewares/not-found.middleware");
 const errorMiddleware = require("../middlewares/error.middleware");
 
@@ -15,6 +17,8 @@ function createApp() {
     app.use(morgan("dev"));
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
+
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
     app.use("/api/v1", routes);
 
